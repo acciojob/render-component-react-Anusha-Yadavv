e5021d483c98d75d13a2dd4c2b1ea696287dda0b
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-11 14:14:47 +0000

    rendering component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Message from './components/Message';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Message/>
+
+    </div>
   )
 }
 
---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+const Message=()=>{
+    return(
+        <div>
+            <p>I am learning React. My life is getting better.</p>
+        </div>
+    )
+}
+
+export default Message;
