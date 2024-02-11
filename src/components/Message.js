c7626bbd44f4f82415e1c06d3113b3c915162e9c
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-11 14:22:57 +0000

    adding h1 tag

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,11 @@
 const Message=()=>{
     return(
         <div>
+            <h1>
             <p>I am learning React. My life is getting better.</p>
+
+
+            </h1>
         </div>
     )
 }
